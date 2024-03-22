abc9a7fb7e5d820eaccdc3e459a8fa049a30ca03
Theoutrace <prakashkumar.imw@gmail.com>
2024-03-23 00:52:50 +0530

    -- script custom value

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+var customValue = document.currentScript.getAttribute(
+  "data-custom-value-web-chat-app"
+);
+
 function prepareFrame() {
+  console.log(customValue);
   var iframe = document.createElement("iframe");
   iframe.setAttribute("src", "https://main--sitewebchat.netlify.app/");
   iframe.setAttribute("style", "position: fixed;bottom: 0;right: 0;");
+  iframe.setAttribute("type", "text/js");
   document.body.appendChild(iframe);
 }
 prepareFrame();
