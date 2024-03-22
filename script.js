35d1fbd220a732425dbff15b3bd5dfeeed329b07
Theoutrace <prakashkumar.imw@gmail.com>
2024-03-23 00:55:34 +0530

    -- customValue added in react app url

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var customValue = document.currentScript.getAttribute(
 );
 
 function prepareFrame() {
-  console.log(customValue);
   var iframe = document.createElement("iframe");
-  iframe.setAttribute("src", "https://main--sitewebchat.netlify.app/");
+  iframe.setAttribute(
+    "src",
+    `https://main--sitewebchat.netlify.app/${customValue}`
+  );
   iframe.setAttribute("style", "position: fixed;bottom: 0;right: 0;");
   iframe.setAttribute("type", "text/js");
   document.body.appendChild(iframe);
