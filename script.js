11b507d29e7fedabf3b395d4f9145f8655c8fb62
Theoutrace <prakashkumar.imw@gmail.com>
2024-03-23 02:03:40 +0530

    --

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-var customValue = document.currentScript.getAttribute(
-  "data-custom-value-web-chat-app"
-);
-
 function prepareFrame() {
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute(
-    "src",
-    `https://main--sitewebchat.netlify.app/?client=${customValue}`
-  );
-  iframe.setAttribute("style", "position: fixed;bottom: 0;right: 0;");
-  iframe.setAttribute("type", "text/js");
-  iframe.setAttribute("id", "iframe_web_chat_id");
-  document.body.appendChild(iframe);
-  document
-    .getElementById("iframe_web_chat_id")
-    .children.contentWindow.postMessage(
-      window.location.href,
-      `https://main--sitewebchat.netlify.app`
+  // Retrieve parent site URL
+  var parentSiteURL = window.location.href;
+
+  try {
+    // Create iframe element
+    var iframe = document.createElement("iframe");
+
+    // Set iframe attributes
+    iframe.setAttribute(
+      "src",
+      `https://main--sitewebchat.netlify.app/?parentURL=${encodeURIComponent(
+        parentSiteURL
+      )}`
     );
+    iframe.setAttribute("style", "position: fixed; bottom: 0; right: 0;");
+    iframe.setAttribute("id", "iframe_web_chat_id");
+
+    // Append iframe to the document body
+    document.body.appendChild(iframe);
+  } catch (error) {
+    console.error("Error while creating iframe:", error);
+  }
 }
+
+// Call the function to prepare the frame
 prepareFrame();
