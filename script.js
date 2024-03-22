c3a85fd2fee3893913b1de56d630f6de0a74c862
Theoutrace <prakashkumar.imw@gmail.com>
2024-03-23 00:57:01 +0530

    customValue in search params

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function prepareFrame() {
   var iframe = document.createElement("iframe");
   iframe.setAttribute(
     "src",
-    `https://main--sitewebchat.netlify.app/${customValue}`
+    `https://main--sitewebchat.netlify.app/?client=${customValue}`
   );
   iframe.setAttribute("style", "position: fixed;bottom: 0;right: 0;");
   iframe.setAttribute("type", "text/js");
